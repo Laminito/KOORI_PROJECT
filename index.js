e32122632d68234d9b4b2ba7cad6e35b1ce6cb93
Laminito <mba25116@gmail.com>
2022-11-28 07:18:43 +0000

    onrender

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config()
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors')
@@ -8,23 +9,29 @@ const dotenv = require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const model = require('./models')
 const apiRouter = require('./routes/apiRouter');
-const user = require('./routes/userRoute');
+const index = require('./routes/index');
 const cont = require('./controllers/userCtrl');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const rateLimit = require('express-rate-limit')
+const errorHandler = require('./middleware/error')
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 Mins
+    max: 100,
+})
 
 
 const server = express();
 const PORT = process.env.PORT || 3001
-const HOST = "localhost";
-const API_SERVICE_URL = "https://psmilab.onrender.com";
 
-
+server.use(limiter);
+server.set('trust proxy', 1);
+// Error handler middleware
+server.use(errorHandler)
 server.use(cookieParser())
-
-
-
 
 server.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -53,22 +60,22 @@ server.use(bodyParser.json());
 //Entrypoint de mon swagger
 server.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-const memoryStore = new session.MemoryStore();
-server.use(session({
-    secret: 'KWhjV<T=-*VW<;cC5Y6U-{F.ppK+])Ub',
-    resave: false,
-    saveUninitialized: true,
-    store: memoryStore
-}));
+// const memoryStore = new session.MemoryStore();
+// server.use(session({
+//     secret: 'KWhjV<T=-*VW<;cC5Y6U-{F.ppK+])Ub',
+//     resave: false,
+//     saveUninitialized: true,
+//     store: memoryStore
+// }));
 
 //EndPoint
-server.use('/api', apiRouter);
-server.use('/', user);
+// server.use('/api', apiRouter);
+server.use('/api', index);
 
 // Info GET endpoint
-server.use('/test', (req, res, next) => {
-    res.send('This is a proxy service which proxies to Billing and Account APIs.');
-});
+// server.use('/test', (req, res, next) => {
+//     res.send('This is a proxy service which proxies to Billing and Account APIs.');
+// });
 
 // Authorization
 // server.use('', (req, res, next) => {
@@ -80,24 +87,24 @@ server.use('/test', (req, res, next) => {
 // });
 
 // Proxy endpoints
-server.use('/proxy', createProxyMiddleware({
-    target: API_SERVICE_URL,
-    changeOrigin: true,
-    pathRewrite: {
-        [`^/proxy`]: '',
-    },
-}));
+// server.use('/proxy', createProxyMiddleware({
+//     target: API_SERVICE_URL,
+//     changeOrigin: true,
+//     pathRewrite: {
+//         [`^/proxy`]: '',
+//     },
+// }));
 
 // Start the Proxy
-server.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
-    model.sequelize.sync({ force: false }).then(() => {
-        console.log("model has been re sync")
-    })
-});
+// server.listen(PORT, HOST, () => {
+//     console.log(`Starting Proxy at ${HOST}:${PORT}`);
+//     model.sequelize.sync({ force: false }).then(() => {
+//         console.log("model has been re sync")
+//     })
+// });
 
 
-// server.listen(PORT, () => console.log(`Server is connected on ${PORT}`))
-// model.sequelize.sync({ force: false }).then(() => {
-//     console.log("model has been re sync")
-// })
+server.listen(PORT, () => console.log(`Server is connected on ${PORT}`))
+model.sequelize.sync({ force: false }).then(() => {
+    console.log("model has been re sync")
+})
